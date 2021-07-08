ac5a919906a01a5c9adb7536d451581791a28298
dansulaimanub <87076536+dansulaimanub@users.noreply.github.com>
2021-07-08 14:29:22 +0100

    Add files via upload

---
+++ Duty Allocation And Optimazation System/assign.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   
+
+    <!-- Title Page-->
+    <title>Assign Duty | Admin Panel | Duty Allocation And Optimization System</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <h1>DAS</h1>
+            <ul id="navli">
+                <li><a class="homeblack" href="aloginwel.php">HOME</a></li>
+                <li><a class="homeblack" href="addemp.php">Add Staff</a></li>
+                <li><a class="homeblack" href="viewemp.php">View Staff</a></li>
+                <li><a class="homered" href="assign.php">Assign Duty</a></li>
+                <li><a class="homeblack" href="assignproject.php">Project Status</a></li>
+                <li><a class="homeblack" href="salaryemp.php">Salary Table</a></li> 
+                <li><a class="homeblack" href="empleave.php">Staff Leave</a></li>
+                <li><a class="homeblack" href="alogin.html">Log Out</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <div class="divider"></div>
+
+
+
+
+    <div class="page-wrapper bg-blue p-t-100 p-b-100 font-robo">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-1">
+                <div class="card-heading"></div>
+                <div class="card-body">
+                    <h2 class="title">Assign Duty</h2>
+                    <form action="process/assignp.php" method="POST" enctype="multipart/form-data">
+
+
+                        
+
+                         <div class="input-group">
+                            
+<input class="input--style-1" type="text" placeholder="Staff ID" name="eid" required="required">
+                        
+</div>
+
+
+
+
+
+                        <div class="input-group">
+                            
+<input class="input--style-1" type="text" placeholder="Staff Duty" name="pname" required="required">
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    
+<input class="input--style-1" type="date" placeholder="date" name="duedate" required="required">
+ 
+
+<input class="input--style-1" type="date" placeholder="date" name="enddate" required="required">
+
+                                  
+                                </div>
+                            </div>
+                            
+                        </div>
+                        
+                        
+
+
+
+                        <div class="p-t-20">
+                            <button class="btn btn--radius btn--green" type="submit">Assign Duty</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
